8bbd29bda95de8511a3c63d541e550fcf18d0383
OliaPetrova <skopindom8@gmail.com>
2024-08-06 21:21:09 +0300

    Add files via upload

---
+++ src/Person.java
@@ -0,0 +1,87 @@
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    private OptionalInt age;
+    private String address;
+
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = OptionalInt.of(age);
+    }
+
+    public boolean hasAge() {
+        return age.isPresent();
+    }
+    public boolean hasAddress() {
+        return address != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void happyBirthday() {
+        if(hasAge()) {
+        age = OptionalInt.of(age.getAsInt()+1);
+        }
+
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(this.surname)
+                .setAddress(this.address)
+                .setAge(0);
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + (hasAge() ? ", возраст: "+ age.getAsInt() : "") + (hasAddress() ? ", адрес: " + address : "");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+
+        if (!name.equals(person.name)) return false;
+        if (!surname.equals(person.surname)) return false;
+        if (age.isPresent() ? !age.equals(person.age) : person.age.isPresent()) return false;
+        return address != null ? address.equals(person.address) : person.address == null;}
+
+    @Override
+    public int hashCode() {
+      int result = name.hashCode();
+      result = 31 * result + surname.hashCode();
+      result = 31 * result + (age.isPresent() ? age.getAsInt() : 0);
+      result = 31 * result + (address != null ? address.hashCode() : 0);
+      return result;
+    }
+}
+
